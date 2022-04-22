46e94505bd8c227b520818132b06fae2fa988718
rjangid9080 <grut.rjangid9080@gmail.com>
2022-04-22 22:38:27 +0530

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import Logo from "./Components/Logo";
+import SignIn from "./Components/SignIn";
 
 function App() {
   return (
-    <div className="text-6xl">
-      <Logo/>
-    </div>
+    <section className="w-full min-h-screen flex flex-col bg-gray-100 justify-center items-center">
+      <div className="w-full mb-4 flex flex-col items-center text-6xl">
+        <Logo />
+        <p className="text-base text-gray-700 font-semibold">
+          fastest way to message !
+        </p>
+      </div>
+      <div className="w-full flex justify-center">
+        <SignIn />
+      </div>
+    </section>
   );
 }
 
